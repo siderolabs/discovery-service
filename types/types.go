d3fd1f324d7af60f2ddf425c9c2cf9b9743e6d62
Seán C McCord <ulexus@gmail.com>
2021-05-09 13:06:10 -0700

    add Name to Node
    Signed-off-by: Seán C McCord <ulexus@gmail.com>

--- types/types.go
+++ types/types.go
@@ -2,9 +2,18 @@ package types
 
 // Node describes a Wireguard Peer
 type Node struct {
+	// Name is the human-readable identifier of this Node.
+	// Usually, this is the kubernetes Node name.
+	// It *should* generally be unique, but it is not required to be so.
+	Name string
+
+	// ID is the unique identifier for this Node.
+	// Usually, this is the Wireguard Public Key of the Node.
    ID string
 
+	// IP is the Wireguard interface IP of this Node.
 	IP string
 
+	// KnownEndpoints is a list of known endpoints (host:port) for this Node.
    KnownEndpoints []string
 }
